8addce42dab2691236ee2e81e4e0f6b4c248d715
Mohammad Tinati <114297179+M-Tinati@users.noreply.github.com>
2026-05-29 20:34:30 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+string FilePath = @"C:\Users\Tinati\Desktop\C++";   //آدرس دایرکتوری
+string[] FormatFiles = Directory.GetFiles(FilePath , "*.cpp"); //آرایه ای که فایل های مسیر داده شده رو میگیره و فرمت مورد نظرمون رو بهش میدیم
+if(FormatFiles.Length> 0) //اگر تعداد فایل بزرگ تر از  صفر بود 
+{
+    Console.WriteLine($"Path -> {FilePath} has cpp count {FormatFiles.Length}"); //در مسیر داده شده به تعداد (مثلا) یک عدد فایل با فرمت مورد نظر وجود دارد
+}
+else
+{
+    Console.WriteLine("Empty");
+}
